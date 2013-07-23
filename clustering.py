acf746dbe63be61322eed30c5dc5768cacc93962
dtphan <pdthanh06@gmail.com>
2013-07-23 08:10:22 +0200

    allez

--- clustering.py
+++ clustering.py
@@ -215,3 +215,80 @@ def read_matrix_data(ifile):
 
 
         
+############################### KM-Clustering ########################################
+class KMClustering(SubspaceClustering, MeasureMixin):
+    """ basically it is a subspace clustering but might contain some additonal information such as a list of measures/values"""
+    def __init__(self, algorithm, parameters, run,
+                 clustering_id = None, **measures):
+
+        SubspaceClustering.__init__(self, algorithm, parameters, run, clustering_id,
+                                    contains_noise = False, clustering_on_dimension = False)
+        MeasureMixin.__init__(self, **measures)
+
+######################################################################################
+""" useful helper functions """
+def my_combi(data, dimensions, projection_dimension_size, nbr_step = 50, number_of_clusters = 4, threshold = 1e-5):
+    """ at each yield, return a k-combination project from a data of n-dimensions """
+    from itertools import combinations
+    for projection in combinations(dimensions, projection_dimension_size):
+        yield data[:,projection], projection, nbr_step, number_of_clusters, threshold
+
+
+######################################  K-MEANS ################################################
+def clusters_from_code(code, nbr_of_cluster, dims):
+    import numpy as np
+    clusters = []    
+    dims = set(dims)
+    for label in xrange(nbr_of_cluster):
+        objs = set(np.where(code == label)[0])
+        #print np.where(code == label)
+        cluster = SubspaceCluster(clustering_id = None, objects = objs, dimensions = dims)
+        clusters.append(cluster) 
+    
+    return clusters
+        
+def kmeans(features, projection, ite = 50, k = 4, threshold = 1e-5):    
+    """ perform k_keamns clustering and return a the result as a subsapce clustering object """
+    from scipy.cluster.vq import kmeans, vq
+    import datetime
+
+    from measures import spatial_coherence
+    
+    #features = dims_arg       
+    
+    centroids, distance = kmeans(features, k, iter=ite, thresh=threshold)
+    code, _ = vq(features, centroids)
+
+
+    run_ = datetime.datetime.now().strftime("%y_%m_%d_%H_%M")
+    params = "projection_size=%d, k=%d" %(len(projection), k)
+    km_clt = KMClustering(algorithm ="exhaustive_kmeans", parameters = params, run = run_, clustering_id = None)
+    clusters = clusters_from_code(code, k, projection)
+    for cluster in clusters:
+        km_clt.add_cluster(cluster)
+
+    measures = {'spatial_coherence': spatial_coherence(km_clt, len(features))[0], 'distortion': distance}
+    km_clt.update_measures(measures)
+    
+    return  km_clt 
+
+def kmeans_args(params):
+    data, projection, ite, k, threshold = params
+    return kmeans(data, projection, ite, k, threshold)
+
+def kmeans_execute(data, dimensions_size, number_of_clusters = 4, processes = 7):    
+    import multiprocessing
+    """ the data here is an numpy multi-array"""
+    
+    pool = multiprocessing.Pool(processes)
+
+    dimensions = np.shape(data)[1]    
+    features = my_combi(data, xrange(dimensions), dimensions_size)      
+    rs = pool.map(kmeans_args, features)    
+    pool.close()
+    pool.join()
+    
+    return rs
+
+
+        
--- dominance.py
+++ dominance.py
@@ -1,27 +1,29 @@
-import * from measures
+from measures import * 
 
 ##################### SIMILARITY ########################
 def cluster_structural_similarity(cluster1, cluster2):
     """ compute the similarity between cluster1 and cluster2. basically it is a pair of object 
-    similarity and dimension similarity. and by similarity we mean precision """
+    similarity and dimension similarity. and by similarity we mean precision. """
+    
     object_similarity = precision_sets(cluster1.objects, cluster2.objects)
     dimension_similarity = precision_sets(cluster1.dimensions, cluster2.dimensions)
 
-    return (object_similarity, dimensions_similarity)
+    return (object_similarity, dimension_similarity)
 
-def is_structurally_similar_to(ref_cluster, target_cluster, object_threshold = 0.5, dimension_threshold = 0.5):
+def is_structurally_similar(ref_cluster, target_cluster, object_threshold = 0.5, dimension_threshold = 0.5):
     """ checks whether the ref cluster is simlar to target cluster, w.r.t the two threshold. this relation is
-    based on the calculation of precision, thus not being symetric nor transitive """
-    object_similarity, dimensions_similarity = cluster_structural_similarity(ref_cluster, target_cluster)
-    return object_similarity >= object_threshold and dimensions_similarity >= dimension_threshold
+    based on the calculation of precision, thus not being symetric nor transitive. they have belong to the same clustering i.e clustering_ids equal """
+    if ref_cluster.clustering_id != target_cluster.clustering_id: return False #clustering_id equality
+    object_similarity, dimension_similarity = cluster_structural_similarity(ref_cluster, target_cluster)
+    return object_similarity >= object_threshold and dimension_similarity >= dimension_threshold
 
-def is_inferior_to(ref_cluster, target_cluster, func):
+def is_inferior(ref_cluster, target_cluster, func):
     """ check whether a ref_cluser is inferior to another one w.r.t a evaluator f """
-    return func(ref_cluster) < target_cluster
+    return func(ref_cluster) < func(target_cluster)
 
-def is_dominated_by(ref_cluster, target_cluster, func, objs_threshold = 0.5, dims_threshold = 0.5):
+def is_dominated(ref_cluster, target_cluster, func, objs_threshold = 0.5, dims_threshold = 0.5):
     """ check whether a ref_cluser is inferior to another one w.r.t a evaluator f """
-    return is_inferior_to(ref_cluster, target_cluster, func) and  is_structurally_similar_to(ref_cluster, target_cluster, object_threshold, dimension_threshold))
+    return is_inferior(ref_cluster, target_cluster, func) and  is_structurally_similar(ref_cluster, target_cluster, objs_threshold, dims_threshold)
 
 ############################################## DOMINANCE #########################################
 def non_dominated_clusters(clusters, func, dim_thres = 0.5, obj_thres = 0.5, filtered = False):
@@ -33,33 +35,46 @@ def non_dominated_clusters(clusters, func, dim_thres = 0.5, obj_thres = 0.5, fil
     2. among dominated: find the those who are not being by any of A, called B
     3. the result is the merge of A&B"""
     
-    clusters = clustering.clusters
+    #clusters = clustering.clusters
     sorted(clusters, key = func, reverse = True)
     sz = len(clusters) # nbr of clusters contained in this clustering
-    idx = 0*np.ones([sz]) # a sz-size array of zeroes
 
     dominance = {}
     for i in xrange(sz):
         clA = clusters[i]
         for j in xrange(i+1, sz):
             clB = clusters[j]                 
-            if is_dominated_by(clB, clA, func, dim_thres, obj_thres):
+            if is_dominated(clB, clA, func, dim_thres, obj_thres):
                 dominance.setdefault(j, set())
                 dominance[j].add(i)
 
-    dominated_candidates = dominance.keys()
-    non_dominated = [i for i in xrange(sz) if i not in dominated]
-    for cand in dominated_candidates:
+    dominated_candidate_idx = dominance.keys(); 
+    non_dominated_idx = [i for i in xrange(sz) if i not in dominated_candidate_idx]; 
+   
+        #print 'dominants: %s' %(non_dominated_idx)
+        
+    for cand in dominated_candidate_idx:
         selected = True
-        for non_dom in non_dominated:
-            if is_dominated_by(cand, non_dom, func, dim_thres, obj_thres):
-                selected = False
+        for non_dom in non_dominated_idx:
+            if is_dominated(clusters[cand], clusters[non_dom], func, dim_thres, obj_thres):
+                selected = False                
                 break
         if selected:
-            non_dominated.append(cand)
-                    
-    return non_dominated
+            non_dominated_idx.append(cand)
+            
+    non_dominated = [clusters[i] for i in non_dominated_idx]
+    return non_dominated_idx, non_dominated
 
 def non_dominated_clustering(clusterings, func, dim_thres = 0.5, obj_thres = 0.5, filtered = False):
     """ patch redundancy filtering for a list of clusterings """
     pass
+
+
+
+
+
+######################## SOME EVALUATOR FUNCTIONS ##################################
+def cardinality_func(cluster):
+    """ a func which compute the interestingness in term of product of cardinality of object times(x) the cardinality of dimensions"""
+    return float(len(cluster.objects)*len(cluster.dimensions))
+
